f1964fe572c140fdef712915242267dd27859fe4
Ikedinobi Onyeka <ikedinobicruz7@gmail.com>
2023-05-04 16:43:16 +0100

    init middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,33 @@
 import express, { Application } from "express";
-
+import { PORT } from "./config";
+import { logger } from "./utils/logger";
+import ErrorMiddleWare from "./middlewares/error.middleware";
+import MorganMiddleware from "./middlewares/morgan.middleware";
 
 export default class App {
 
     private app: Application
+    private port: string | number
 
     constructor () {
-        this.app = express()
+        this.app = express();
+        this.port = PORT || 8080;
+        this.initializeMiddlewares();
+        this.initializeErrorHandling();
     }
 
     public listen(): void {
-        this.app.listen(8000, () => {
-            console.log(`[server] server running @ http:localhost:8000`)
+        this.app.listen(this.port, () => {
+            logger.info(`[server] server running @ http://localhost:${this.port}`)
         })
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(MorganMiddleware.createInstance)
+    }
+
+    private initializeErrorHandling() {
+        this.app.use(ErrorMiddleWare.handleErrors);
     }
 
 }
